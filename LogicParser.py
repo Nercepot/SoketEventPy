fd9c76f6c6f8b5163e74f9aa0955020e3690c721
Nercepot <43406598+Nercepot@users.noreply.github.com>
2024-07-26 06:39:16 +0300

    Add files via upload

--- LogicParser.py
+++ LogicParser.py
@@ -15,8 +15,30 @@ def parser_yml_config_password():
         config = yaml.safe_load(file)
         return config.get('password', [])
 
+def parser_yml_config_promo():
+    with open("config.yml", 'r') as file:
+        config = yaml.safe_load(file)
+        return config.get('promo', [])
+
+def parser_yml_config_twich_price():
+    with open("config.yml", 'r') as file:
+        config = yaml.safe_load(file)
+        return config.get('twich', [])
+
+def parser_yml_config_price():
+    with open("config.yml", 'r') as file:
+        config = yaml.safe_load(file)
+        return config.get('price', [])
+
+def parser_yml_config_price_promo():
+    with open("config.yml", 'r') as file:
+        config = yaml.safe_load(file)
+        return config.get('price_promo', [])
+
 ip = parser_yml_config_ip()
 port = parser_yml_config_port()
 password = parser_yml_config_password()
-
-
+promo = parser_yml_config_promo()
+twich_price = parser_yml_config_twich_price()
+price = parser_yml_config_price()
+price_promo = parser_yml_config_price_promo()
--- SoketEvent.py
+++ SoketEvent.py
@@ -2,8 +2,7 @@ import parsTokens
 import socketio
 import json
 from mcrcon import MCRcon
-from LogicParser import ip, password, port
-
+from LogicParser import ip, password, port, promo, twich_price, price, price_promo
 
 tokens = parsTokens.load_tokens_from_yaml('token.yml')
 
@@ -22,24 +21,25 @@ for token in tokens:
         amount = y['amount']
         currency = y['currency']
         message = y['message']
+        billing = y['billing_system']
 
-        print(username)
-
-        if (float(amount) == 100.00):
-            command = "twl add " + username + "30:d"
-            return command
-        elif (float(amount) == 3000.00):
-            command = "twl add " + username + "30:d"
-            return command
+        if (builtins == 'moneymailru'):
+            if (message == promo):
+                if (float(amount) == price_promo):
+                    command = "twl add " + username + "30:d"
+                    return command
+            elif (float(amount) == price):
+                command = "twl add " + username + "30:d"
+                return command
+        elif (builtins == 'TWITCH'):
+            if (float(amount) == twich_price):
+                command = "twl add " + username + "30:d"
+                return command
 
     mc.connect()
-    mc.command(on_message())
+    mc.command(on_command())
 
     sio.connect('wss://socket.donationalerts.ru:443', transports='websocket')
 
-while True:
-    on_connect()
-
-
-    
-
+    while True:
+        on_connect()
